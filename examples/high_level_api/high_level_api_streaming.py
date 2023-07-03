8fb668d1dd4be5c7a5c489948ff2a88910594230
chendixi05 <chendixi05@kuaishou.com>
2023-07-03 21:54:50 +0800

    example -p argument

--- examples/high_level_api/high_level_api_streaming.py
+++ examples/high_level_api/high_level_api_streaming.py
@@ -5,13 +5,14 @@ from llama_cpp import Llama
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--model", type=str, default="../models/7B/ggml-models.bin")
+parser.add_argument("-p", "--prompt", type=str, default="Question: What are the names of the planets in the solar system? Answer: ")
 args = parser.parse_args()
 
 llm = Llama(model_path=args.model)
 
 stream = llm(
-    "Question: What are the names of the planets in the solar system? Answer: ",
-    max_tokens=48,
+    args.prompt,
+    max_tokens=128,
     stop=["Q:"],
     stream=True,
 )
